01a2cc966e8e5eda1759dc33d7fdf011cc41d61e
dhq_boiler <dhq_boiler@live.jp>
2020-10-29 21:37:01 +0900

    Update version info.

--- Homura/Properties/AssemblyInfo.cs
+++ Homura/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Homura")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©dhq_boiler 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
