8548b5dd0e1dabc779894aa7d7aca4b5a2779a8a
dhq_boiler <dhq_boiler@live.jp>
2022-09-09 23:53:27 +0900

    add test

--- Homura.Test/VersionControl/TableMigrationTest.cs
+++ Homura.Test/VersionControl/TableMigrationTest.cs
@@ -9,7 +9,9 @@ using Homura.Test.TestFixture.Entity;
 using Homura.Test.TestFixture.Migration;
 using Homura.Test.TestFixture.Migration.Plan;
 using NUnit.Framework;
+using Sunctum.Domain.Data.Dao.Migration.Plan;
 using System;
+using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
 using System.Linq;
@@ -516,7 +518,43 @@ namespace Sunctum.Infrastructure.Test.IntegrationTest.Data.Rdbms.VersionControl
             }
         }
 
-        [TearDown]
+        [Test]
+        public void ByVersion_Page_SimpleCase()
+        {
+            var svManager = new DataVersionManager();
+            svManager.CurrentConnection = ConnectionManager.DefaultConnection;
+            svManager.Mode = VersioningStrategy.ByTick;
+            var registeringPlan = new ChangePlanByVersion<VersionOrigin>();
+            registeringPlan.AddVersionChangePlan(new PageChangePlan_VersionOrigin());
+            svManager.RegisterChangePlan(registeringPlan);
+            svManager.SetDefault();
+            svManager.UpgradeToTargetVersion();
+
+            using (var conn = new SQLiteConnection($"Data Source={_filePath}"))
+            {
+                conn.Open();
+
+                var dao = new PageDao(typeof(VersionOrigin));
+
+                dao.Insert(new Page()
+                {
+                    ID = Guid.Parse("89B5FA63-7D91-4622-8DB1-61F7BE80B416"),
+                    BookID = Guid.Parse("75813CCD-CC48-4894-ACC6-3CE8C5333422"),
+                    ImageID = Guid.Parse("655CAD5B-423D-4531-B474-68983FFFE385"),
+                    PageIndex = 3,
+                    Title = "E9C012E2-938A-484F-8F01-04169B920781",
+                });
+
+                var record = dao.FindBy(new Dictionary<string, object>() { { "ID", Guid.Parse("89B5FA63-7D91-4622-8DB1-61F7BE80B416") } }).SingleOrDefault();
+                Assert.That(record, Is.Not.Null);
+                Assert.That(record, Has.Property("BookID").EqualTo(Guid.Parse("75813CCD-CC48-4894-ACC6-3CE8C5333422")));
+                Assert.That(record, Has.Property("ImageID").EqualTo(Guid.Parse("655CAD5B-423D-4531-B474-68983FFFE385")));
+                Assert.That(record, Has.Property("PageIndex").EqualTo(3));
+                Assert.That(record, Has.Property("Title").EqualTo("E9C012E2-938A-484F-8F01-04169B920781"));
+            }
+        }
+
+            [TearDown]
         public void TearDown()
         {
             if (File.Exists(_filePath))
