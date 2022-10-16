ac9f82c469c608f1a284948b8c2a7328780207d1
dhq_boiler <dhq_boiler@live.jp>
2022-10-16 20:38:17 +0900

    fix and add tests

--- Homura.Test/UnitTest/DirectQueryTest.cs
+++ Homura.Test/UnitTest/DirectQueryTest.cs
@@ -10,26 +10,70 @@ namespace Homura.Test.UnitTest
     [TestFixture]
     public class DirectQueryTest
     {
-        private string _filePath = "DirectQueryTest";
+        private string _filePath = "DirectQueryTest.db";
 
         [Test]
         public void RunQuery()
         {
-            DirectQuery.RunQuery(new SQLiteConnection($"Data Source={_filePath}"), (conn) =>
+            using (var conn = new SQLiteConnection($"Data Source={_filePath}"))
             {
-                string sql = $"create table if not exists Alpha(ID NUMERIC PRIMARY KEY, Title TEXT NOT NULL, AuthorID NUMERIC, PublishDate NUMERIC, ByteSize INTEGER)";
-                conn.Execute(sql);
-            });
+                conn.Open();
+                DirectQuery.RunQuery(conn, (conn) =>
+                {
+                    string sql = $"create table if not exists Alpha(ID NUMERIC PRIMARY KEY, Title TEXT NOT NULL, AuthorID NUMERIC, PublishDate NUMERIC, ByteSize INTEGER)";
+                    conn.Execute(sql);
+                });
+            }
+        }
+
+        [Test]
+        public void RunQuery_long()
+        {
+            using (var conn = new SQLiteConnection($"Data Source={_filePath}"))
+            {
+                conn.Open();
+                var ret = DirectQuery.RunQuery<long>(conn, (conn) =>
+                {
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "select 1";
+                        return (long)cmd.ExecuteScalar();
+                    }
+                });
+                Assert.That(ret, Is.EqualTo(1));
+            }
         }
 
         [Test]
         public async Task RunQueryAsync()
         {
-            await DirectQuery.RunQueryAsync(new SQLiteConnection($"Data Source={_filePath}"), async (conn) =>
+            using (var conn = new SQLiteConnection($"Data Source={_filePath}"))
             {
-                string sql = $"create table if not exists Alpha(ID NUMERIC PRIMARY KEY, Title TEXT NOT NULL, AuthorID NUMERIC, PublishDate NUMERIC, ByteSize INTEGER)";
-                await conn.ExecuteAsync(sql);
-            });
+                conn.Open();
+                await DirectQuery.RunQueryAsync(conn, async (conn) =>
+                {
+                    string sql = $"create table if not exists Alpha(ID NUMERIC PRIMARY KEY, Title TEXT NOT NULL, AuthorID NUMERIC, PublishDate NUMERIC, ByteSize INTEGER)";
+                    await conn.ExecuteAsync(sql);
+                });
+            }
+        }
+
+        [Test]
+        public async Task RunQueryAsync_long()
+        {
+            using (var conn = new SQLiteConnection($"Data Source={_filePath}"))
+            {
+                await conn.OpenAsync();
+                var ret = await DirectQuery.RunQueryAsync<long>(conn, async (conn) =>
+                {
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "select 1";
+                        return (long) await cmd.ExecuteScalarAsync();
+                    }
+                });
+                Assert.That(ret, Is.EqualTo(1));
+            }
         }
 
         [TearDown]
